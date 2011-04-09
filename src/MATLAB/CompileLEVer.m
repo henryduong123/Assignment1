01e9b2907bb3159d861e8e6c2e58114381ced393
Eric Wait <eric@waitphoto.com>
2011-04-09 12:49:12 -0500

    Should have been with last

---
+++ src/MATLAB/CompileLEVer.m
@@ -0,0 +1,6 @@
+mcc -m LEVer.m -d ..\..\bin\.
+mcc -m Segmentor.m
+system('copy Segmentor.exe ..\..\bin\.');
+if(isempty(dir('.\MTC.exe')) || isempty(dir('..\..\bin\MTC.exe')))
+    warndlg('Make sure that MTC.exe is in the same dir as LEVer.exe and LEVer MATLAB src code');
+end
