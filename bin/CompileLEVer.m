15306e48622fda7585a8ff2d084b92f1f44c772c
Eric Wait <eric@waitphoto.com>
2011-04-09 12:48:39 -0500

    Moved Compile Code to SRC dir
    Should place the .exe files into the bin dir

--- bin/CompileLEVer.m
+++
@@ -1,6 +0,0 @@
-mcc -m ..\LEVer.m
-mcc -m ..\Segmentor.m
-system('copy Segmentor.exe ..\.');
-if(isempty(dir('./MTC.exe')))
-    warndlg('Make sure that MTC.exe is in the same dir as LEVer.exe');
-end
