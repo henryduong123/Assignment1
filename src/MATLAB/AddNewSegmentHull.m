dd6849b13f4ff41999b761bc98b2a06462bb1f4f
Mark Winter <mwinter@drexel.edu>
2011-04-24 17:20:28 -0500

    Attempt to avoid old segmentation intersections when adding a new cell hull.

--- src/MATLAB/AddNewSegmentHull.m
+++ src/MATLAB/AddNewSegmentHull.m
@@ -11,6 +11,10 @@ function newTrackID = AddNewSegmentHull(clickPt)
     
     oldTracks = [HashedCells{Figures.time}.trackID];
     
+    if ( ~isempty(newObj) )
+        newObj = makeNonOverlapping(newObj, Figures.time, clickPt);
+    end
+    
     if ( isempty(newObj) )
         % Add a point hull since we couldn't find a segmentation containing the click
         newHull.time = Figures.time;
@@ -34,3 +38,52 @@ function newTrackID = AddNewSegmentHull(clickPt)
     
 %     newTrackID = [CellFamilies(newFamilyIDs).rootTrackID];
 end
+
+%TODO: Maybe handle convex hull intersection instead of interior points as
+%the current method still allows considerable hull overlap in some cases.
+function newobj = makeNonOverlapping(obj, t, clickPt)
+    global CONSTANTS CellHulls HashedCells
+    
+    newobj = [];
+    
+    ccidxs = vertcat(CellHulls([HashedCells{t}.hullID]).indexPixels);
+    pix = obj.indexPixels;
+    
+    bPickPix = ~ismember(pix, ccidxs);
+    
+    if ( all(bPickPix) )
+        newobj = obj;
+        return;
+    end
+    
+    bwimg = zeros(CONSTANTS.imageSize);
+    bwimg(pix(bPickPix)) = 1;
+    
+    CC = bwconncomp(bwimg,8);
+    if ( CC.NumObjects < 1 )
+        newobj = [];
+        return;
+    end
+    
+    for i=1:CC.NumObjects
+        [r c]=ind2sub(size(bwimg),CC.PixelIdxList{i});
+        try
+            ch = convhull(r,c);
+        catch errmsg
+            continue;
+        end
+        
+        if ( inpolygon(clickPt(1), clickPt(2), c(ch), r(ch)) )
+            bCCPix = ismember(pix, CC.PixelIdxList{i});
+            
+            newobj.indexPixels = CC.PixelIdxList{i};
+            newobj.imagePixels = obj.imagePixels(bCCPix);
+            
+            newobj.points = [c(ch) r(ch)];
+            newobj.centerOfMass = mean([r c]);
+            
+            break;
+        end
+    end
+end
+
