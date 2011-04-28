df2f6b9e0aa548375eeff0d77eed3bb18bdf5e6e
Mark Winter <mwinter@drexel.edu>
2011-04-28 09:25:23 -0500

    Modified CompileLEVer to build c Release binaries as well.

--- src/MATLAB/CompileLEVer.m
+++ src/MATLAB/CompileLEVer.m
@@ -1,4 +1,26 @@
 tic
+
+vstoolroot = getenv('VS90COMNTOOLS');
+if ( isempty(vstoolroot) )
+    error('Cannot compile MTC and mexMAT without Visual Studio 2008');
+end
+
+% buildDirList = {'..\c\MTC\Release' '..\c\MTC\Intermediate' '..\c\mexMAT\Release' '..\c\mexMAT\Intermediate'};
+% 
+% for i=1:length(buildDirList)
+%     if ( exist(buildDirList{i},'dir') )
+%         rmdir(buildDirList{i}, 's');
+%     end
+% end
+
+system(['"' fullfile(vstoolroot,'..','IDE','devenv.com') '"' ' /build Release "..\c\MTC.sln"']);
+system(['"' fullfile(vstoolroot,'..','IDE','devenv.com') '"' ' /build Release "..\c\mexMAT.sln"']);
+
+% clears out mex cache so src/mexMAT.mexw32 can be overwritten
+clear mex
+system('copy ..\c\mexMAT\Release\mexMAT.dll .\mexMAT.mexw32');
+system('copy ..\c\MTC\Release\MTC.exe ..\..\bin\');
+
 mcc -m LEVer.m -d ..\..\bin\.
 mcc -m Segmentor.m
 system('copy Segmentor.exe ..\..\bin\.');
