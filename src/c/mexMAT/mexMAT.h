ee3c3c4ef1c2dc99353a838ceb65f4dfd2a1a204
Mark Winter <mwinter@drexel.edu>
2011-05-05 14:20:20 -0500

    Added copyright notice to all c/c++ source and headers.

--- src/c/mexMAT/mexMAT.h
+++ src/c/mexMAT/mexMAT.h
@@ -1,3 +1,10 @@
+//******************************************************
+//
+//    This file is part of LEVer.exe
+//    (C) 2011 Andrew Cohen, Eric Wait and Mark Winter
+//
+//******************************************************
+
 #include "mex.h"
 
 #include <map>
