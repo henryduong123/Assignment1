868e0714219596aa7d22e14f4adf73677b002c2c
Eric Wait <eric@waitphoto.com>
2011-05-04 20:32:22 -0500

    Change Label fix

--- src/MATLAB/ChangeLabel.m
+++ src/MATLAB/ChangeLabel.m
@@ -38,8 +38,8 @@ if(~isempty(CellTracks(oldTrackID).childrenTracks) && ~isempty(CellTracks(newTra
 end
 
 for i=1:length(hulls)
-    if((newTrackHashedTime+i-1>0 && newTrackHashedTime+i-1<length(CellTracks(newTrackID).hulls)) ||...
-            CellTracks(newTrackID).hulls(newTrackHashedTime+i-1)~=0),break,end;
+    if(newTrackHashedTime+i-1<0 || (newTrackHashedTime+i-1<length(CellTracks(newTrackID).hulls) &&...
+            CellTracks(newTrackID).hulls(newTrackHashedTime+i-1)~=0)),break,end;
     if(hulls(i)~=0)
         AddHullToTrack(hulls(i),newTrackID,[]);
     end
