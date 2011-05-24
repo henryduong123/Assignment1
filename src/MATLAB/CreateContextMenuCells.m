51cf5518541f39a8965d9625830ef577ad28e35d
Mark Winter <mwinter@drexel.edu>
2011-05-23 19:10:22 -0500

    Reverted and created AITPD branch for these changes:
    "AITPD now will change track's phenotype depending on clusters returned"

--- src/MATLAB/CreateContextMenuCells.m
+++ src/MATLAB/CreateContextMenuCells.m
@@ -99,12 +99,12 @@ PhenoMenu = uimenu(Figures.cells.contextMenuHandle,...
 
 uimenu(PhenoMenu,...
     'Label',        'Create new phenotype...',...
-    'CallBack',     @Phenotypes);
+    'CallBack',     @phenotypes);
 
 for i=1:length(CellPhenotypes.descriptions)
     CellPhenotypes.contextMenuID(i)=uimenu(PhenoMenu,...
         'Label',        CellPhenotypes.descriptions{i},...
-        'CallBack',     @Phenotypes);
+        'CallBack',     @phenotypes);
 end
 
 
@@ -445,6 +445,97 @@ function properties(src,evnt)
 if(isempty(trackID)),return,end
 
 ContextProperties(hullID,trackID);
+end
+
+% added 4 19 2011 ac
+function phenotypes(src,evnt)
+
+global Figures CellPhenotypes CellTracks
+
+[hullID trackID] = GetClosestCell(0);
+if(isempty(trackID)),return,end
+% which did they click
+for i=1:length(CellPhenotypes.contextMenuID)
+    if src == CellPhenotypes.contextMenuID(i)
+        break;
+    end
+end
+
+if src~=CellPhenotypes.contextMenuID(i)
+    % add new one
+    NewPhenotype=inputdlg('Enter description for new phenotype','Cell Phenotypes');
+    if isempty(NewPhenotype)
+        return
+    end
+    
+    PhenoMenu = get(CellPhenotypes.contextMenuID(1),'parent');
+    i=length(CellPhenotypes.descriptions)+1;
+    CellPhenotypes.contextMenuID(i)=uimenu(PhenoMenu,...
+        'Label',        NewPhenotype{1},...
+        'CallBack',     @phenotypes);  
+    CellPhenotypes.descriptions(i)=NewPhenotype;  
+
+    
+end
+bActive = strcmp(get(CellPhenotypes.contextMenuID(i),'checked'),'on');
+if 1==i
+    % death!
+    if CellTracks(trackID).phenotype
+        set(CellPhenotypes.contextMenuID(CellTracks(trackID).phenotype),'checked','off');
+    end
+    if bActive
+        CellTracks(trackID).phenotype=0;
+    else
+        CellTracks(trackID).phenotype=1;
+    end
+    
+    if bActive || ~isempty(CellTracks(trackID).timeOfDeath )
+        % turn off death...
+        CellTracks(trackID).timeOfDeath = [];
+        History('Push');
+        try
+            ProcessNewborns(CellTracks(trackID).familyID);
+        catch errorMessage
+            try
+                ErrorHandeling(['ProcessNewborns(' num2str(trackID) ')-- ' errorMessage.message],errorMessage.stack);
+                return
+            catch errorMessage2
+                fprintf('%s',errorMessage2.message);
+                return
+            end
+        end
+        LogAction(['Removed death for ' num2str(trackID)],[],[]);
+        DrawTree(Figures.tree.familyID);
+        DrawCells();
+    else
+        markDeath(src,evnt);       
+    end 
+    return
+end
+
+if bActive
+    History('Push');
+    set(CellPhenotypes.contextMenuID(CellTracks(trackID).phenotype),'checked','off');
+    CellTracks(trackID).phenotype=0;
+    LogAction(['Deactivated phenotype ' CellPhenotypes.descriptions{i} ' for track ' num2str(trackID)]);
+else    
+    History('Push');
+    if CellTracks(trackID).phenotype        
+        set(CellPhenotypes.contextMenuID(CellTracks(trackID).phenotype),'checked','off');
+        LogAction(['Deactivated phenotype ' CellPhenotypes.descriptions{CellTracks(trackID).phenotype} ' for track ' num2str(trackID)]);
+    end
+    if 1==CellTracks(trackID).phenotype        
+        CellTracks(trackID).timeOfDeath = [];
+        DrawCells();
+    end
+    
+    CellTracks(trackID).phenotype=i;
+    set(CellPhenotypes.contextMenuID(CellTracks(trackID).phenotype),'checked','on');
+    LogAction(['Activated phenotype ' CellPhenotypes.descriptions{i} ' for track ' num2str(trackID)]);
+
+end
+DrawTree(Figures.tree.familyID);   
+    
 end
 
 function phenoPopulate(src,evnt)
