041d1fd047d0445a854422b2e26e4fef1d34ef63
Mark Winter <mwinter@drexel.edu>
2013-01-23 19:27:40 -0500

    GetNearestTrackHull not returning correct hash index when searching forward within a track.

--- src/MATLAB/+Helper/GetNearestTrackHull.m
+++ src/MATLAB/+Helper/GetNearestTrackHull.m
@@ -1,21 +1,24 @@
-% hull = GetNearestTrackHull(trackID, time, searchDir)
+% [hull hullTime] = GetNearestTrackHull(trackID, time, searchDir)
 % Search track for a non-zero hull nearest to the specified time, searchDir
 % beack in time if searchDir < 0 or forward if searchDir > 0. Returns 0 if
 % no hull exists in specified direction.
 
-function hull = GetNearestTrackHull(trackID, time, searchDir)
-    global CellTracks
+function [hull hullTime] = GetNearestTrackHull(trackID, time, searchDir)
+    global CellTracks CellHulls
     
     hull = 0;
+    hullTime = 0;
     
     hash = time - CellTracks(trackID).startTime + 1;
     if ( hash < 1 && (searchDir >= 0) )
         hull = CellTracks(trackID).hulls(1);
+        hullTime = CellHulls(hull).time;
         return;
     end
     
     if ( (hash > length(CellTracks(trackID).hulls)) && ((searchDir < 0)) )
         hull = CellTracks(trackID).hulls(end);
+        hullTime = CellHulls(hull).time;
         return;
     end
     
@@ -26,7 +29,7 @@ function hull = GetNearestTrackHull(trackID, time, searchDir)
     hull = CellTracks(trackID).hulls(hash);
     if ( hull == 0 )
         if ( searchDir >= 0 )
-            hidx = find(CellTracks(trackID).hulls(hash:end), 1, 'first');
+            hidx = find(CellTracks(trackID).hulls(hash:end), 1, 'first') + (hash - 1);
         else
             hidx = find(CellTracks(trackID).hulls(1:hash), 1, 'last');
         end
@@ -36,5 +39,6 @@ function hull = GetNearestTrackHull(trackID, time, searchDir)
         end
         
         hull = CellTracks(trackID).hulls(hidx);
+        hullTime = CellHulls(hull).time;
     end
 end
