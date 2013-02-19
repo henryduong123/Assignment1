78ef9743f9d81a5c758a1010a8968dde742cde61
Walt Mankowski <wmankowski.coe.drexel.edu>
2013-02-19 16:10:29 -0500

    dummy function to ensure than fluoresence markers get added to the undo stack
    This should really only be an issue when they're added to a tree that's already
    been edited.

---
+++ src/MATLAB/+Editor/AddHematoFluor.m
@@ -0,0 +1,9 @@
+% [historyAction] = AddHematoFluor()
+% Edit Action:
+% 
+% This is just a dummy function so that the flourescence markers will be
+% added to the undo stack.
+
+function [historyAction] = AddHematoFluor(dummy)
+    historyAction = 'Push';
+end
