5a16f51ed34ee5104251cf00a5886a38716b4d63
Walt Mankowski <wmankowski.coe.drexel.edu>
2013-02-19 15:57:25 -0500

    added code to find intersections with fluorescence images

--- src/MATLAB/+Tracker/HematoTracker.m
+++ src/MATLAB/+Tracker/HematoTracker.m
@@ -1,5 +1,5 @@
 function HematoTracker()
-    global CONSTANTS CellHulls HashedCells ConnectedDist FluorData
+    global CONSTANTS CellHulls HashedCells ConnectedDist FluorData CellTracks HaveFluor
     
     bKeep = (~[CellHulls.deleted]);
     CellHulls = CellHulls(bKeep);
@@ -13,21 +13,6 @@ function HematoTracker()
     for i=1:length(CellHulls)
         HashedCells{CellHulls(i).time} = [HashedCells{CellHulls(i).time} struct('hullID',{i}, 'trackID',{0})];
     end
-    
-    fprintf(1,'Computing intersections with fluorescence images...');
-    for t=1:tmax
-        hulls = [HashedCells{t}(:).hullID];
-        greenInd = FluorData(t).greenInd;
-        for i=1:length(hulls)
-            inter = intersect(CellHulls(hulls(i)).indexPixels, greenInd);
-            if (isempty(inter))
-                CellHulls(hulls(i)).greenInd = [];
-            else
-                CellHulls(hulls(i)).greenInd = greenInd;
-            end
-        end
-    end
-    fprintf(1, 'Done\n');
     
     ConnectedDist = [];
     fprintf(1,'Calculating Distances...');
@@ -56,4 +41,50 @@ function HematoTracker()
     fprintf('Finalizing Data...');
     Tracker.RebuildTrackingData(objTracks, gConnect);
     fprintf('Done\n');
+
+    fprintf(1,'Computing intersections with fluorescence images...');
+    tic;
+    for t=1:tmax
+        hulls = [HashedCells{t}(:).hullID];
+        greenInd = FluorData(t).greenInd;
+        for i=1:length(hulls)
+            inter = intersect(CellHulls(hulls(i)).indexPixels, greenInd);
+            if (isempty(inter))
+                CellHulls(hulls(i)).greenInd = [];
+            else
+                CellHulls(hulls(i)).greenInd = greenInd;
+            end
+        end
+    end
+    fprintf(1, 'Done, %f sec\n', toc);
+    
+    fprintf(1,'Checking tracks for fluorescence...');
+    tic;
+    flTimes = find(HaveFluor);
+    for i=1:length(CellTracks)
+        times = CellTracks(i).startTime:CellTracks(i).endTime;
+        CellTracks(i).markerTimes = intersect(times,flTimes);
+        if(~isempty(CellTracks(i).markerTimes))
+            wasGreen = 0;
+            for j=1:size(CellTracks(i).markerTimes,2)
+                hullID = Tracks.GetHullID(CellTracks(i).markerTimes(1,j),i);
+                if (hullID <= 0)
+                    CellTracks(i).markerTimes(2,j) = wasGreen;
+                    continue;
+                elseif (~isempty(CellHulls(hullID).greenInd))
+                    inter = intersect(CellHulls(hullID).greenInd,CellHulls(hullID).indexPixels);
+                    if (length(inter)>length(CellHulls(hullID).indexPixels)*0.3)
+                        CellTracks(i).markerTimes(2,j) = 1;
+                        wasGreen = 1;
+                        continue;
+                    end
+                end
+                CellTracks(i).markerTimes(2,j) = 0;
+                wasGreen = 0;
+            end
+        end
+    end
+    fprintf(1, 'Done, %f sec\n', toc);
+
+    
 end
