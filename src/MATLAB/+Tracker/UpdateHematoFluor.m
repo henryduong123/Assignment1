388a49e6877f94d8907eb30bd69901b04da9090d
Walt Mankowski <wmankowski.coe.drexel.edu>
2013-02-19 16:18:31 -0500

    recomputes the fluorescence markers
    This is called when a cell is edited, and checks all the tracks that are active
    at time t.

---
+++ src/MATLAB/+Tracker/UpdateHematoFluor.m
@@ -0,0 +1,69 @@
+function UpdateHematoFluor(t)
+    global CellHulls HashedCells FluorData CellTracks HaveFluor
+
+    fprintf(1, 'Running UpdateHematoFluor()\n');
+    fprintf(1, 'Clearing out old fluorescence indicators...');
+    tic;
+    hulls = [HashedCells{t}(:).hullID];
+    for i=1:length(hulls)
+        CellHulls(hulls(i)).greenInd = [];
+    end
+    
+%     for i=1:length(CellTracks)
+%         CellTracks(i).markerTimes = {};
+%     end
+    fprintf(1, 'Done, %f sec\n', toc);
+    
+    fprintf(1,'Computing intersections with fluorescence images...');
+    tic;
+    hulls = [HashedCells{t}(:).hullID];
+    greenInd = FluorData(t).greenInd;
+    for i=1:length(hulls)
+        inter = intersect(CellHulls(hulls(i)).indexPixels, greenInd);
+        if (isempty(inter))
+            CellHulls(hulls(i)).greenInd = [];
+        else
+            CellHulls(hulls(i)).greenInd = greenInd;
+        end
+    end
+    fprintf(1, 'Done, %f sec\n', toc);
+    
+    fprintf(1,'Checking tracks for fluorescence...');
+    tic;
+    flTimes = find(HaveFluor);
+    for i=1:length(CellTracks)
+        if (i == 18386)
+            i = 18386;
+        end
+        if (isempty(CellTracks(i).startTime) || isempty(CellTracks(i).endTime))
+            continue;
+        end
+        if (t < CellTracks(i).startTime || t > CellTracks(i).endTime)
+            continue;
+        end
+        times = CellTracks(i).startTime:CellTracks(i).endTime;
+        CellTracks(i).markerTimes = intersect(times,flTimes);
+        if(~isempty(CellTracks(i).markerTimes))
+            wasGreen = 0;
+            for j=1:size(CellTracks(i).markerTimes,2)
+                hullID = Tracks.GetHullID(CellTracks(i).markerTimes(1,j),i);
+                if (hullID <= 0)
+                    CellTracks(i).markerTimes(2,j) = wasGreen;
+                    continue;
+                elseif (~isempty(CellHulls(hullID).greenInd))
+                    inter = intersect(CellHulls(hullID).greenInd,CellHulls(hullID).indexPixels);
+                    if (length(inter)>length(CellHulls(hullID).indexPixels)*0.3)
+                        CellTracks(i).markerTimes(2,j) = 1;
+                        wasGreen = 1;
+                        continue;
+                    end
+                end
+                CellTracks(i).markerTimes(2,j) = 0;
+                wasGreen = 0;
+            end
+        end
+        CellTracks(i).fluorTimes = CellTracks(i).markerTimes;
+    end
+    fprintf(1, 'Done, %f sec\n', toc);
+
+end
