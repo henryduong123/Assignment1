e9237145daa2d2ca72227fa96c969f8a98637406
Mark Winter <mwinter@drexel.edu>
2013-04-02 14:07:14 -0400

    Actually add colors when creating a new phenotype. Uses the "bright" track color table.

--- src/MATLAB/+Editor/AddPhenotype.m
+++ src/MATLAB/+Editor/AddPhenotype.m
@@ -6,13 +6,46 @@
 % undo stack it will always be followed by a ContextSetPhenotype action.
 
 function [historyAction phenoID] = AddPhenotype(description)
-    global CellPhenotypes
+    global CellPhenotypes Colors
+    
+    if ( isempty(Colors) )
+        Load.CreateColors();
+    end
+    
+    % Find all colors that haven't been used yet
+    phenoColors = vertcat(CellPhenotypes.colors);
+    allowedColors = getAllowedColors(phenoColors);
+    
+    newColorIdx = randi(size(allowedColors,1));
+    newPhenoColor = allowedColors(newColorIdx,:);
     
     CellPhenotypes.descriptions = [CellPhenotypes.descriptions description];
+    CellPhenotypes.colors = [CellPhenotypes.colors; newPhenoColor];
     
     phenoID = length(CellPhenotypes.descriptions);
     
     UI.UpdatePhenotypeMenu();
     
     historyAction = '';
+end
+
+function [allowedColors bAllowedColors] = getAllowedColors(phenoColors)
+    global Colors
+    
+    if ( isempty(Colors) )
+        Load.CreateColors();
+    end
+    
+    bAllowedColors = false(size(Colors,1),1);
+    for i=1:size(Colors,1)
+        dists = sum((ones(size(phenoColors,1),1)*Colors(i,1:3) - phenoColors).^2, 2);
+        minDist = min(dists);
+        
+        % A dubious threshold, but less than anything in the table
+        if ( minDist > (0.1^2) )
+            bAllowedColors(i) = 1;
+        end
+    end
+    
+    allowedColors = Colors(bAllowedColors, 1:3);
 end
