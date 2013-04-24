fcb50cd1f097225bee5619943c10c310bb678f64
Walt Mankowski <wmankowski.coe.drexel.edu>
2013-04-24 12:23:05 -0400

    Catch cases where we oversegment very faint fluorescence images
    Only count it as a "real" segmentation if the number of fluorescent pixels we
    found is below a threshold. This is because the Michel algorithm doesn't work
    well for very faint images where it can't find any strong gradients.

--- src/MATLAB/+Segmentation/HematoSegmentation.m
+++ src/MATLAB/+Segmentation/HematoSegmentation.m
@@ -103,7 +103,16 @@ if (isfield(CONSTANTS, 'rootFluorFolder'))
         % find all the fluorescence pixels in the image
         fluor = Helper.LoadIntensityImage(filename);
         [bw] = Segmentation.Michel(fluor, [3 3]);
-        FluorData(i).greenInd = find(bw);
+        
+        % some early fluor frames are so faint that Michel oversegments
+        % them
+        w = find(bw);
+        wPct = numel(w) / numel(bw(:));
+        if wPct < 0.1
+            FluorData(i).greenInd = w;
+        else
+            FluorData(i).greenInd = {};
+        end
     end
         
 end
