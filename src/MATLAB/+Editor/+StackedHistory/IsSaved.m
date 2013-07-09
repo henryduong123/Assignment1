0c103ea74540b6367cf7540508b941d2cf84967a
Mark Winter <mwinter@drexel.edu>
2013-07-08 20:51:59 -0400

    Fixed CanUndo/CanRedo/IsSaved so that they execute appropriately even on an uninitialized history.

--- src/MATLAB/+Editor/+StackedHistory/IsSaved.m
+++ src/MATLAB/+Editor/+StackedHistory/IsSaved.m
@@ -2,6 +2,11 @@
 function bSaved = IsSaved()
     global HistoryStack
     
+    bSaved = true;
+    if ( isempty(HistoryStack) )
+        return;
+    end
+    
     if ( HistoryStack.level > 1 )
         bSaved = 1;
         return;
