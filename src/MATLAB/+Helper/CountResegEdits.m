2243da905f3605ded32a3890f70b2a31fd98301b
Mark Winter <mwinter@drexel.edu>
2013-09-30 13:49:52 -0400

    Slightly more robust reseg count (in case reseg ended badly).

--- src/MATLAB/+Helper/CountResegEdits.m
+++ src/MATLAB/+Helper/CountResegEdits.m
@@ -1,6 +1,6 @@
 
-function [userEditCount resegEditCount userEditList resegEditList] = CountResegEdits()
-    global ReplayEditActions
+function [userEditCount resegEditCount userEditList resegEditList] = CountResegEdits(trackID)
+    global ReplayEditActions CellHulls CellTracks CellFamilies
     
     userEditCount = 0;
     resegEditCount = 0;
@@ -14,6 +14,8 @@ function [userEditCount resegEditCount userEditList resegEditList] = CountResegE
     typeStack = 1;
     stackEnds = 0;
     editStack = {{}};
+    
+    totalReseg = 0;
     
     bInReseg = 0;
     for i=1:length(ReplayEditActions)
@@ -29,6 +31,8 @@ function [userEditCount resegEditCount userEditList resegEditList] = CountResegE
             
             numEdits = length(ReplayEditActions(i).ret{2}.SegEdits);
             
+            totalReseg = totalReseg + numEdits;
+            
             newEdit = struct('count',{numEdits}, 'type',{typeStack(end)}, 'editFunc',{'ResegEdits'});
             editStack{end} = [editStack{end}(1:stackEnds(end)); {newEdit}];
 
@@ -39,86 +43,95 @@ function [userEditCount resegEditCount userEditList resegEditList] = CountResegE
         % TODO: This is unfortunate, and I need it to really just not happen
         if ( bInReseg && strcmpi(ReplayEditActions(i).funcName,'Editor.InitHistory') )
             warning('RESEG:NoFinish', 'The current file was closed without finishing resegmentation process, edit counts may be inaccurate');
-            return;
+%             return;
         end
         
-        % Increase stack level and pick an edit type if a new subtask is started
-        if ( strcmpi(ReplayEditActions(i).funcName,'Editor.StartReplayableSubtask') )
-            typeStack = [typeStack 1];
-            stackEnds = [stackEnds 0];
-            editStack = [editStack; {{}}];
-            
-            if ( strcmpi(ReplayEditActions(i).args{1},'PauseResegTask') )
-                typeStack(end) = 3;
-            elseif ( strcmpi(ReplayEditActions(i).args{1},'InteractiveResegTask') )
-                typeStack(end) = 2;
-            end
-            continue;
-        end
-        
-        % Reduce stack level and add new edit block to level below (if not empty)
-        if ( strcmpi(ReplayEditActions(i).funcName,'Editor.StopReplayableSubtask') )
-            topStack = editStack{end};
-            topEnd = stackEnds(end);
-            
-            stackEnds = stackEnds(1:(end-1));
-            typeStack = typeStack(1:(end-1));
-            editStack = editStack(1:(end-1));
-            
-            if ( topEnd > 0 )
-                popEntry = topStack(1:topEnd,:);
-                editStack{end} = [editStack{end}(1:stackEnds(end)); {popEntry}];
-                stackEnds(end) = stackEnds(end) + 1;
-            end
-            continue;
-        end
-        
-        % Just drop stack level by one
-        if ( strcmpi(ReplayEditActions(i).funcName,'Editor.DropReplayableSubtask') )
-            
-            stackEnds = stackEnds(1:(end-1));
-            typeStack = typeStack(1:(end-1));
-            editStack = editStack(1:(end-1));
-            
-            if ( strcmpi(ReplayEditActions(i).args{1},'PauseResegTask') )
-                bInPause = 0;
-            end
-            continue;
-        end
-        
-        % Ignore these actions as they have no direct edit significance
-        if ( strcmpi(ReplayEditActions(i).funcName,'Editor.ResegPlayAction') || strcmpi(ReplayEditActions(i).funcName,'Editor.ResegPauseAction') ...
-                || strcmpi(ReplayEditActions(i).funcName,'Editor.ResegBackAction') || strcmpi(ReplayEditActions(i).funcName,'Editor.ResegPauseAction') ...
-                || strcmpi(ReplayEditActions(i).funcName,'Editor.Top') )
-            continue;
-        end
-        
-        % Undo reduces current edit end by one
-        if ( strcmpi(ReplayEditActions(i).funcName,'Editor.Undo') )
-            if ( stackEnds(end) > 0 )
-                stackEnds(end) = stackEnds(end) - 1;
-            end
-            
-            continue;
-        end
-        
-        % Redo increases current edit end by one
-        if ( strcmpi(ReplayEditActions(i).funcName,'Editor.Redo') )
-            if ( stackEnds(end) < size(editStack{end},1) )
-                stackEnds(end) = stackEnds(end) + 1;
-            end
-            
-            continue;
-        end
-        
-        newEdit = struct('count',{1}, 'type',{typeStack(end)}, 'editFunc',{ReplayEditActions(i).funcName});
-        editStack{end} = [editStack{end}(1:stackEnds(end)); {newEdit}];
-        
-        stackEnds(end) = stackEnds(end) + 1;
+%         % Increase stack level and pick an edit type if a new subtask is started
+%         if ( strcmpi(ReplayEditActions(i).funcName,'Editor.StartReplayableSubtask') )
+%             typeStack = [typeStack 1];
+%             stackEnds = [stackEnds 0];
+%             editStack = [editStack; {{}}];
+%             
+%             if ( strcmpi(ReplayEditActions(i).args{1},'PauseResegTask') )
+%                 typeStack(end) = 3;
+%             elseif ( strcmpi(ReplayEditActions(i).args{1},'InteractiveResegTask') )
+%                 typeStack(end) = 2;
+%             end
+%             continue;
+%         end
+%         
+%         % Reduce stack level and add new edit block to level below (if not empty)
+%         if ( strcmpi(ReplayEditActions(i).funcName,'Editor.StopReplayableSubtask') )
+%             topStack = editStack{end};
+%             topEnd = stackEnds(end);
+%             
+%             stackEnds = stackEnds(1:(end-1));
+%             typeStack = typeStack(1:(end-1));
+%             editStack = editStack(1:(end-1));
+%             
+%             if ( topEnd > 0 )
+%                 popEntry = topStack(1:topEnd,:);
+%                 editStack{end} = [editStack{end}(1:stackEnds(end)); {popEntry}];
+%                 stackEnds(end) = stackEnds(end) + 1;
+%             end
+%             continue;
+%         end
+%         
+%         % Just drop stack level by one
+%         if ( strcmpi(ReplayEditActions(i).funcName,'Editor.DropReplayableSubtask') )
+%             
+%             stackEnds = stackEnds(1:(end-1));
+%             typeStack = typeStack(1:(end-1));
+%             editStack = editStack(1:(end-1));
+%             
+%             if ( strcmpi(ReplayEditActions(i).args{1},'PauseResegTask') )
+%                 bInPause = 0;
+%             end
+%             continue;
+%         end
+%         
+%         % Ignore these actions as they have no direct edit significance
+%         if ( strcmpi(ReplayEditActions(i).funcName,'Editor.ResegPlayAction') || strcmpi(ReplayEditActions(i).funcName,'Editor.ResegPauseAction') ...
+%                 || strcmpi(ReplayEditActions(i).funcName,'Editor.ResegBackAction') || strcmpi(ReplayEditActions(i).funcName,'Editor.ResegPauseAction') ...
+%                 || strcmpi(ReplayEditActions(i).funcName,'Editor.Top') )
+%             continue;
+%         end
+%         
+%         % Undo reduces current edit end by one
+%         if ( strcmpi(ReplayEditActions(i).funcName,'Editor.Undo') )
+%             if ( stackEnds(end) > 0 )
+%                 stackEnds(end) = stackEnds(end) - 1;
+%             end
+%             
+%             continue;
+%         end
+%         
+%         % Redo increases current edit end by one
+%         if ( strcmpi(ReplayEditActions(i).funcName,'Editor.Redo') )
+%             if ( stackEnds(end) < size(editStack{end},1) )
+%                 stackEnds(end) = stackEnds(end) + 1;
+%             end
+%             
+%             continue;
+%         end
+%         
+%         newEdit = struct('count',{1}, 'type',{typeStack(end)}, 'editFunc',{ReplayEditActions(i).funcName});
+%         editStack{end} = [editStack{end}(1:stackEnds(end)); {newEdit}];
+%         
+%         stackEnds(end) = stackEnds(end) + 1;
     end
     
-    [resegEditCount resegEditList] = countAllEdits(editStack{1}, 2);
-    [userEditCount userEditList] = countAllEdits(editStack{1}, 3);
+%     [resegEditCount resegEditList] = countAllEdits(editStack{1}, 2);
+%     [userEditCount userEditList] = countAllEdits(editStack{1}, 3);
+
+    famID = CellTracks(trackID).familyID;
+    trackHulls = [CellTracks(CellFamilies(famID).tracks).hulls];
+    nzHulls = trackHulls(trackHulls ~= 0);
+    
+    bUserEdited = [CellHulls(nzHulls).userEdited];
+
+    userEditCount = nnz(bUserEdited);
+    resegEditCount = totalReseg;
 end
 
 function [editCount editList] = countAllEdits(editStack, editType)
