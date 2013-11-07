5e6939f85ffcbe5098d1364062037a7cec048bbd
Maria Enokian <mce29@drexel.edu>
2013-11-07 17:58:11 -0500

    Swapping Fully works with:
    descriptions
    hull IDs
    Colors

--- src/MATLAB/+Load/FixOldFileVersions.m
+++ src/MATLAB/+Load/FixOldFileVersions.m
@@ -85,27 +85,32 @@ function bNeedsUpdate = FixOldFileVersions()
         if (amb)
             y = CellPhenotypes.descriptions(2);
             c = CellPhenotypes.colors(2,:);
+            NotOffscreen = CellPhenotypes.hullPhenoSet(2,:);
+            Noffscreen = find(CellPhenotypes.hullPhenoSet(2,:) == NotOffscreen);
+            OffScreen = find(CellPhenotypes.hullPhenoSet(2,:) == 2);
             if(~(strcmp(CellPhenotypes.descriptions{2},'ambiguous')))
                 CellPhenotypes.descriptions(i) = y;
                 CellPhenotypes.colors(i,:) = c;
                 CellPhenotypes.descriptions(2) = {'ambiguous'};
                 CellPhenotypes.colors(2,:) = [.549 .28235 .6235];
+                CellPhenotypes.hullPhenoSet(2,Noffscreen)= NotOffscreen;
+                CellPhenotypes.hullPhenoSet(2,OffScreen)= 2;
             end
         end
         
         if (ofscr)
             y = CellPhenotypes.descriptions(3);
             c = CellPhenotypes.colors(3,:);
-            valx = CellPhenotypes.hullPhenoSet(2,:);
-            x = find(CellPhenotypes.hullPhenoSet(2,:) == valx);
-            z = find(CellPhenotypes.hullPhenoSet(2,:) == 3);
+            NotOffscreen = CellPhenotypes.hullPhenoSet(2,:);
+             Noffscreen = find(CellPhenotypes.hullPhenoSet(2,:) == NotOffscreen);
+             OffScreen = find(CellPhenotypes.hullPhenoSet(2,:) == 3);
             if(~(strcmp(CellPhenotypes.descriptions{3},'off screen')))
                 CellPhenotypes.descriptions(i) = y;
                 CellPhenotypes.colors(i,:) = c;
-                CellPhenotypes.hullPhenoSet(2,z)= i;
                 CellPhenotypes.descriptions(3) = {'off screen'};
                 CellPhenotypes.colors(3,:) = [0 1 1];
-                CellPhenotypes.hullPhenoSet(2,x)= 3;
+                CellPhenotypes.hullPhenoSet(2,Noffscreen)= NotOffscreen;
+                CellPhenotypes.hullPhenoSet(2,OffScreen)= 3;
                 
             end
             
