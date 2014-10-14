45e6059b97ff6c87f161672ec0c681e7b64b4d42
Mark Winter <mwinter@drexel.edu>
2014-10-14 16:43:46 -0400

    Removed old Backtracker tool and dependencies.

--- src/MATLAB/+Backtracker/DeleteTrackingEntry.m
+++
@@ -1,8 +0,0 @@
-function DeleteTrackingEntry(selectedTrackID, dirFlag, time)
-    global bDirty
-    
-    Families.RemoveFromTree(selectedTrackID, time);
-    bDirty = true;
-    
-    Backtracker.UpdateBacktrackInfo();
-end
--- src/MATLAB/+Backtracker/UpdateBacktrackHulls.m
+++
@@ -1,7 +0,0 @@
-function UpdateBacktrackHulls()
-    global CellTracks CellFamilies BackTrackIdx BackSelectHulls
-    
-    hullTracks = unique(Hulls.GetTrackID(BackSelectHulls));
-    familyIDs = unique([CellTracks(hullTracks).familyID]);
-    BackTrackIdx = [CellFamilies(familyIDs).tracks];
-end
