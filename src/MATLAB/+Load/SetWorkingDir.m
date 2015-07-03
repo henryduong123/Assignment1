5f5b2a7909f3d0ba841b3e56e8099facf516ea3f
Mark Winter <mwinter@drexel.edu>
2015-07-03 19:57:57 -0400

    Set working directory to user documents or user profile folder if application is deployed.

--- src/MATLAB/+Load/SetWorkingDir.m
+++ src/MATLAB/+Load/SetWorkingDir.m
@@ -1,8 +1,19 @@
 function SetWorkingDir()
-
-appDir=getenv('APPDATA');
-appDir=[appDir '\LEVER'];
-if ~exist(appDir)
-    mkdir(appDir);
+    userDir = getenv('USERPROFILE');
+    if ( isempty(userDir) )
+        return;
+    end
+    
+    docsDir = fullfile(userDir,'Documents');
+    if ( ~exist(docsDir,'dir') )
+        cd(userDir);
+        return;
+    end
+    
+    leverDir = fullfile(docsDir,'LEVER');
+    if ( ~exist(leverDir) )
+        mkdir(leverDir);
+    end
+    
+    cd(leverDir);
 end
-cd(appDir);
