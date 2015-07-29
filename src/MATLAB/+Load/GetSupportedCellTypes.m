6bb2c600f6c78086ebcc60f13a127decce1021e6
Mark Winter <mwinter@drexel.edu>
2015-07-29 18:17:30 -0400

    Removed WEHI cell type and segmentation.

--- src/MATLAB/+Load/GetSupportedCellTypes.m
+++ src/MATLAB/+Load/GetSupportedCellTypes.m
@@ -7,14 +7,8 @@ function SupportedTypes = GetSupportedCellTypes()
     SupportedTypes(1).trackParams = struct('dMaxCenterOfMass',{40}, 'dMaxConnectComponentTracker',{20});
     SupportedTypes(1).leverParams = struct('timeResolution',{5}, 'maxPixelDistance',{40}, 'maxCenterOfMassDistance',{40}, 'dMaxConnectComponent',{40});
     
-    SupportedTypes(3).name = 'Embryonic';
-    SupportedTypes(3).segParams = struct('imageAlpha',{1.5});
-    SupportedTypes(3).trackParams = struct('dMaxCenterOfMass',{80}, 'dMaxConnectComponentTracker',{40});
-    SupportedTypes(3).leverParams = struct('timeResolution',{10}, 'maxPixelDistance',{80}, 'maxCenterOfMassDistance',{80}, 'dMaxConnectComponent',{40});
-    
-    SupportedTypes(4).name = 'Wehi';
-    SupportedTypes(4).segParams = struct('imageAlpha',{1.5});
-    SupportedTypes(4).trackParams = struct('dMaxCenterOfMass',{80}, 'dMaxConnectComponentTracker',{40});
-    SupportedTypes(4).leverParams = struct('timeResolution',{10}, 'maxPixelDistance',{80}, 'maxCenterOfMassDistance',{80}, 'dMaxConnectComponent',{40});
-    
+    SupportedTypes(2).name = 'Embryonic';
+    SupportedTypes(2).segParams = struct('imageAlpha',{1.5});
+    SupportedTypes(2).trackParams = struct('dMaxCenterOfMass',{80}, 'dMaxConnectComponentTracker',{40});
+    SupportedTypes(2).leverParams = struct('timeResolution',{10}, 'maxPixelDistance',{80}, 'maxCenterOfMassDistance',{80}, 'dMaxConnectComponent',{40});
 end
