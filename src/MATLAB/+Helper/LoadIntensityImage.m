4b637c83e8ce15710a69cae6c2e3ada7f68da4bc
Mark Winter <mwinter@drexel.edu>
2016-04-25 18:54:33 -0400

    Quiet read and never prompt on LoadIntensityImage.

--- src/MATLAB/+Helper/LoadIntensityImage.m
+++ src/MATLAB/+Helper/LoadIntensityImage.m
@@ -6,7 +6,7 @@ function imgray = LoadIntensityImage(frame, chan)
     global CONSTANTS
     bitrates = [8 12 16];
     
-    im = MicroscopeData.Reader(Metadata.GetImageInfo(), frame, chan);
+    im = MicroscopeData.Reader(Metadata.GetImageInfo(), frame, chan,[],[],[],true,false);
     if ( ndims(im) > 3 )
         error('LEVER tool only supports grayscale images!');
     end
